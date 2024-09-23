281d31d48ac98c288d013890d891a2bab1407a3c
chechminh19 <meruem0812@gmail.com>
2024-09-23 12:37:27 +0700

    init

---
+++ Application/Service/AuthenService.cs
@@ -0,0 +1,13 @@
+﻿using Application.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service
+{
+    public class AuthenService : IAuthenService
+    {
+    }
+}
